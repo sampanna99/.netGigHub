532b058b573c22359493764332b14f2947f17d4c
sampanna pokhrel <sampanna99@gmail.com>
2016-09-04 19:55:02 -0500

    create the gig model

--- GigHub/Migrations/Configuration.cs
+++ GigHub/Migrations/Configuration.cs
@@ -5,7 +5,7 @@ namespace GigHub.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<GigHub.Models.ApplicationDbContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<Models.ApplicationDbContext>
     {
         public Configuration()
         {

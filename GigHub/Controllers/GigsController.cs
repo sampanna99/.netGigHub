039d451bfe5db5e8a71d888d2167dfd773d828f3
sampanna pokhrel <sampanna99@gmail.com>
2016-09-08 19:36:10 -0500

    upto update gigs

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -57,11 +57,32 @@ namespace GigHub.Controllers
         {
             var viewModel = new GigFormViewModel
             {
-                Genres = _context.Genres.ToList()
+                Genres = _context.Genres.ToList(),
+                Heading = "Add a Gig"
             };
 
-            return View(viewModel);
+            return View("GigForm", viewModel);
         }
+
+        [Authorize]
+        public ActionResult Edit(int id)
+        {
+            var userId = User.Identity.GetUserId();
+            var gig = _context.Gigs.Single(g => g.ID == id && g.ArtistId == userId);
+            var viewModel = new GigFormViewModel
+            {
+                Id = gig.ID,
+                Genres = _context.Genres.ToList(),
+                Date = gig.DateTime.ToString("d MMM yyyy"),
+                Time = gig.DateTime.ToString("HH:mm"),
+                Genre = gig.GenreId,
+                Venue = gig.Venue,
+                Heading = "Edit a gig"
+            };
+
+            return View("GigForm", viewModel);
+        }
+
         [Authorize]
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -70,7 +91,7 @@ namespace GigHub.Controllers
             if (!ModelState.IsValid)
             {
                 viewModel.Genres = _context.Genres.ToList();
-                return View("Create", viewModel);
+                return View("GigForm", viewModel);
             }
 
             var gig = new Gig
@@ -84,5 +105,28 @@ namespace GigHub.Controllers
             _context.SaveChanges();
             return RedirectToAction("Mine", "Gigs");
         }
+
+
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Update(GigFormViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                viewModel.Genres = _context.Genres.ToList();
+                return View("GigForm", viewModel);
+            }
+            var userId = User.Identity.GetUserId();
+            var gig = _context.Gigs.Single(g => g.ID == viewModel.Id && g.ArtistId == userId);
+            gig.Venue = viewModel.Venue;
+            gig.DateTime = viewModel.GetDateTime();
+            gig.GenreId = viewModel.Genre;
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Mine", "Gigs");
+        }
+
     }
 }
--- GigHub/ViewModels/GigFormViewModel.cs
+++ GigHub/ViewModels/GigFormViewModel.cs
@@ -1,12 +1,17 @@
-﻿using GigHub.Models;
+﻿using GigHub.Controllers;
+using GigHub.Models;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq.Expressions;
+using System.Web.Mvc;
 
 namespace GigHub.ViewModels
 {
     public class GigFormViewModel
     {
+        public int Id { get; set; }
+
         [Required]
         public string Venue { get; set; }
 
@@ -20,11 +25,34 @@ namespace GigHub.ViewModels
 
         [Required]
         public byte Genre { get; set; }
+
         public IEnumerable<Genre> Genres { get; set; }
+
+        public string Heading { get; set; }
+
+        public string Action
+        {
+            get
+            {
+
+                Expression<Func<GigsController, ActionResult>> update = (c => c.Update(this));
+
+                Expression<Func<GigsController, ActionResult>> create = (c => c.Create(this));
+
+                var action = (Id != 0) ? update : create;
+
+                return (action.Body as MethodCallExpression).Method.Name; // action is also expression . we don't want to
+                                                                          // call it like the return below coz if we change name it might fail the program
+                                                                          // so we call it like expression and later we do .method.name
+                                                                          //try to use lamda rather than the one below.
+                                                                          // return (Id != 0) ? "Update" : "Create";
+            }
+        }
+
         public DateTime GetDateTime()
         {
 
-            return DateTime.Parse(string.Format("{0} {1}", Date, Time)); 
+            return DateTime.Parse(string.Format("{0} {1}", Date, Time));
 
         }
     }
